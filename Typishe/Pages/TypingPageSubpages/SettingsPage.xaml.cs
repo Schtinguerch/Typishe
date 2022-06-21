3c9b878947d77826de24232fdf5abaa84d3a7b0e
Schtinguerch <schtinguerch@gmail.com>
2022-06-21 18:46:40 +0500

    Upd: using settings page splitted into 3 settings

---
+++ Typishe/Pages/TypingPageSubpages/SettingsPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TypisheApi;
+
+namespace Typishe.Pages.TypingPageSubpages
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingsPage.xaml
+    /// </summary>
+
+    public partial class SettingsPage : Page, IRequestable
+    {
+        private IRequestable[] _requestablePages = new IRequestable[3];
+        private int _selectedIndex = 0;
+
+        public SettingsPage(SubPage selectedSubPage = SubPage.VisualSetupSettings)
+        {
+            InitializeComponent();
+            OpenPage(selectedSubPage);
+        }
+
+        public void OpenPage(SubPage selectedSubPage)
+        {
+            switch (selectedSubPage)
+            {
+                case SubPage.VisualSetupSettings:
+                    _selectedIndex = 0;
+                    if (_requestablePages[0] == null)
+                    {
+                        _requestablePages[0] = new VisualSetupPage();
+                    }
+
+                    SubpageFrame.Navigate(_requestablePages[0]);
+                    break;
+
+                case SubPage.FunctionalSetupSettings:
+                    _selectedIndex = 1;
+                    //TODO: create the functional setup page
+                    break;
+
+                case SubPage.ExerciseSetupSettings:
+                    _selectedIndex = 2;
+                    //TODO: create the exercise setup page
+                    break;
+            }
+        }
+
+        public void ApplySettings()
+        {
+            _requestablePages[_selectedIndex]?.ApplySettings();
+        }
+
+        public void DiscardSettings()
+        {
+            _requestablePages[_selectedIndex]?.DiscardSettings();
+        }
+
+        public void NewFile()
+        {
+            _requestablePages[_selectedIndex]?.NewFile();
+        }
+
+        public void OpenFile()
+        {
+            _requestablePages[_selectedIndex]?.OpenFile();
+        }
+
+        public void SaveFile()
+        {
+            _requestablePages[_selectedIndex]?.SaveFile();
+        }
+
+        public void SaveFileAs()
+        {
+            _requestablePages[_selectedIndex]?.SaveFileAs();
+        }
+
+        private void OpenVisualSetupButton_Click(object sender, RoutedEventArgs e) => OpenPage(SubPage.VisualSetupSettings);
+        private void OpenFunctionalSetupButton_Click(object sender, RoutedEventArgs e) => OpenPage(SubPage.FunctionalSetupSettings);
+        private void OpenExerciseSetupButton_Click(object sender, RoutedEventArgs e) => OpenPage(SubPage.ExerciseSetupSettings);
+
+        private void NewFileButton_Click(object sender, RoutedEventArgs e) => NewFile();
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e) => OpenFile();
+        private void SaveFileButton_Click(object sender, RoutedEventArgs e) => SaveFile();
+        private void SaveAsFileButton_Click(object sender, RoutedEventArgs e) => SaveFileAs();
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e) => ApplySettings();
+        private void DiscardButton_Click(object sender, RoutedEventArgs e) => DiscardSettings();
+    }
+}
---
+++ Typishe/Pages/TypingPageSubpages/VisualSetupPage.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Typishe.Setup;
+using TypisheApi.Setup;
+
+namespace Typishe.Pages.TypingPageSubpages
+{
+    /// <summary>
+    /// Логика взаимодействия для ApplicationSettingsPage.xaml
+    /// </summary>
+    public partial class VisualSetupPage : Page, IRequestable
+    {
+        private Dictionary<Button, Action> _actionsByButton;
+        private VisualSetup _standardVisualSetup;
+
+        public VisualSetupPage()
+        {
+            InitializeComponent();
+            _standardVisualSetup = ApplicationSetup.VisualSetup.Clone();
+
+            #region SetActionByButtons
+            _actionsByButton = new Dictionary<Button, Action>()
+            {
+                { EditBackgroundBrushButton, () => 
+                {
+                    BrushPicker.ClearEventDelegates();
+                    BrushPicker.Picker.SelectedColorChanged += (brush) =>
+                    {
+                        if (BrushPicker.EnablePreview)
+                        {
+                            ApplicationSetup.VisualSetup.BackgroundBrush = brush;
+                        }
+                    };
+
+                    BrushPicker.ColorAccepted += (brush) => ApplicationSetup.VisualSetup.BackgroundBrush = brush;
+                    BrushPicker.ColorCanceled += (brush) => ApplicationSetup.VisualSetup.BackgroundBrush = brush;
+                    
+                    BrushPicker.Show(ApplicationSetup.VisualSetup.BackgroundBrush);
+                }},
+
+                { EditMenuBrushButton, () =>
+                {
+                    BrushPicker.ClearEventDelegates();
+                    BrushPicker.Picker.SelectedColorChanged += (brush) =>
+                    {
+                        if (BrushPicker.EnablePreview)
+                        {
+                            ApplicationSetup.VisualSetup.MenuBrush = brush;
+                        }
+                    };
+
+                    BrushPicker.ColorAccepted += (brush) => ApplicationSetup.VisualSetup.MenuBrush = brush;
+                    BrushPicker.ColorCanceled += (brush) => ApplicationSetup.VisualSetup.MenuBrush = brush;
+
+                    BrushPicker.Show(ApplicationSetup.VisualSetup.MenuBrush);
+                }},
+
+                { EditControlBackgroundBrushButton, () =>
+                {
+                    BrushPicker.ClearEventDelegates();
+                    BrushPicker.Picker.SelectedColorChanged += (brush) =>
+                    {
+                        if (BrushPicker.EnablePreview)
+                        {
+                            ApplicationSetup.VisualSetup.ControlBackgroundBrush = brush;
+                        }
+                    };
+
+                    BrushPicker.ColorAccepted += (brush) => ApplicationSetup.VisualSetup.ControlBackgroundBrush = brush;
+                    BrushPicker.ColorCanceled += (brush) => ApplicationSetup.VisualSetup.ControlBackgroundBrush = brush;
+
+                    BrushPicker.Show(ApplicationSetup.VisualSetup.ControlBackgroundBrush);
+                }},
+            };
+            #endregion
+        }
+
+        public void ApplySettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DiscardSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveFileAs()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EditBrushOnClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var relativePosition = RelativePoint(button, BackgroundGrid);
+            relativePosition.Y += 48d;
+
+            Canvas.SetTop(BrushPicker, relativePosition.Y);
+            BrushPicker.Show(new SolidColorBrush(Colors.Aqua));
+
+            if (relativePosition.Y > 200d)
+            {
+                VerticalScroller.TargetVerticalOffset = relativePosition.Y - 60d;
+            }
+
+            if (_actionsByButton.TryGetValue(button, out var selectPickerAction))
+            {
+                selectPickerAction();
+            }
+        }
+
+        private void EditFontOnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private Point RelativePoint(UIElement element, UIElement relativeTo)
+        {
+            return element.TransformToAncestor(relativeTo).Transform(new Point(0, 0));
+        }
+    }
+}
