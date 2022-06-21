ec4907b573cad221886ed2249e70476cb82982b0
Schtinguerch <schtinguerch@gmail.com>
2022-06-21 18:47:03 +0500

    Add: interface for setup pages inside the settings page

---
+++ Typishe/Pages/TypingPageSubpages/IRequestable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typishe.Pages.TypingPageSubpages
+{
+    public interface IRequestable
+    {
+        void ApplySettings();
+        void DiscardSettings();
+
+        void NewFile();
+        void OpenFile();
+        void SaveFile();
+        void SaveFileAs();
+    }
+}
