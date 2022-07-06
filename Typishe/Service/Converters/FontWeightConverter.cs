380770bf7dd104637dd967743e6b3e08188bfd6e
Schtinguerch <schtinguerch@gmail.com>
2022-07-06 15:10:36 +0500

    Add: font weight converter from number

---
+++ Typishe/Service/Converters/FontWeightConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Typishe.Service
+{
+    public class FontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var weightValue = (int) parameter.ToString().Parse();
+            return FontWeight.FromOpenTypeWeight(weightValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+    }
+}
