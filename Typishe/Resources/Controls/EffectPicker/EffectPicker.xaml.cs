7e19630fdecdb6c0e6c30fb730fa9f5a43fa01f0
Schtinguerch <schtinguerch@gmail.com>
2022-07-10 17:51:13 +0500

    Add: whole list of effects, event handlers

--- Typishe/Resources/Controls/EffectPicker/EffectPicker.xaml.cs
+++ Typishe/Resources/Controls/EffectPicker/EffectPicker.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Typishe.Resources.Controls.ColorPicker;
 
 namespace Typishe.Resources.Controls
 {
@@ -20,9 +22,38 @@ namespace Typishe.Resources.Controls
     /// </summary>
     public partial class EffectPicker : UserControl
     {
+        public delegate void SelectedEffectChangedHandler(Effect? selectedEffect);
+        public event SelectedEffectChangedHandler SelectedEffectChanged;
+
+        private Effect? _selectedEffect = null;
+
+        public Effect? StandardEffect { get; set; }
+        public Effect? SelectedEffect
+        {
+            get => _selectedEffect;
+            set
+            {
+                _selectedEffect = value;
+                SelectedEffectChanged?.Invoke(value);
+            }
+        }
+
         public EffectPicker()
         {
             InitializeComponent();
+        }
+
+        public void ClearEventDelegates()
+        {
+            if (SelectedEffectChanged == null)
+            {
+                return;
+            }
+
+            foreach (var eventMethod in SelectedEffectChanged.GetInvocationList())
+            {
+                SelectedEffectChanged -= (SelectedEffectChangedHandler)eventMethod;
+            }
         }
     }
 }
